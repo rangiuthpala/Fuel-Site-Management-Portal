4118f51d610240e644032cec0cbfa106ee0a8c7f
Gayanka <gayanka1990@gmail.com>
2023-05-23 23:44:49 +1200

    fuel indicator date pick done

--- src/app/fuel-indicator/fuel-indicator.component.ts
+++ src/app/fuel-indicator/fuel-indicator.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AllservicesService, Fuelindicator } from '../service/allservices.service';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-fuel-indicator',
@@ -11,13 +13,15 @@ export class FuelIndicatorComponent {
 
   fuelindicator: Observable<Fuelindicator[]> | undefined;
 
+  date = new FormControl(new Date());
+
   constructor(private service: AllservicesService) {
     this.reloadData();
   }
 
   reloadData() {
-
-    this.fuelindicator = this.service.getFuelIndicatorlist();
+    const dateNew = this.service.formatDateNew(this.date.value?.toLocaleDateString());
+    this.fuelindicator = this.service.getFuelIndicatorlist(dateNew);
 
   }
 
